85f9d2c88e1764666f615f5bf47f540f7c56b35d
EdinsonJim <edinsonjim@gmail.com>
2017-02-07 14:27:43 +0100

    with params its more dynamics’s

--- basic-control.py
+++ basic-control.py
@@ -3,15 +3,30 @@
 simple g-code streaming script for grbl
 """
 
-# import necessary packages
+# Usage: python basic-control.py --serialport /dev/cu.wchusbserial1420 --commandfile basic-cmd.nc
+
+# import the necessary packages
+import argparse
 import serial
 import time
 
+# construct the argument parse and parse the arguments
+a = argparse.ArgumentParser()
+a.add_argument("-p", "--serialport", required=True,
+    help="Serial Port name")
+
+a.add_argument("-c", "--commandfile", required=True,
+    help="Commands file")
+
+args = vars(a.parse_args())
+
+serialPortName = args["serialport"]
+
 # open grbl serial port
-s = serial.Serial('/dev/cu.wchusbserial1420', 9600)
+s = serial.Serial(serialPortName, 9600)
 
 # open g-code-file
-f = open('basic-cmd.nc', 'r')
+f = open(args["commandfile"], 'r')
 
 # wake up grbl
 s.write("\r\n\r\n")
